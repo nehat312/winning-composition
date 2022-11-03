07152270b28765749cc28ae384e215d9aea38f65
nehat312 <nate.ehat@gmail.com>
2022-11-03 01:57:20 -0400

    Update NBA_champions.py

--- NBA_champions.py
+++ NBA_champions.py
@@ -556,8 +556,8 @@ st.plotly_chart(bar_champions_salary.add_layout_image(
          yref="paper",
          x=0.5,
          y=0.5,
-         sizex=1.9,
-         sizey=1.2,
+         sizex=2,
+         sizey=1,
          sizing="contain", #"stretch"
             opacity=.65,
          xanchor="center",
