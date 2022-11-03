c6d8496582f9af8cdd1ea49f01646bd1957203c8
nehat312 <nate.ehat@gmail.com>
2022-11-03 00:08:56 -0400

    Update NBA_champions.py

--- NBA_champions.py
+++ NBA_champions.py
@@ -539,8 +539,7 @@ WS_col_5.image(MEM_logo, caption='MEM', width=35)
 # bar_champs_salary = bar_champs_salary.update_yaxes(categoryorder='total descending')
 # st.plotly_chart(bar_champions_salary.update_yaxes(categoryorder='category ascending'), use_container_width=True, sharing="streamlit")
 st.plotly_chart(bar_champions_salary.add_layout_image(
-    dict(
-        source=court_img_1,
+    dict(source='images/Court1.png', #court_img_1,
         xref="x",
         yref="y",
             # x=0,
